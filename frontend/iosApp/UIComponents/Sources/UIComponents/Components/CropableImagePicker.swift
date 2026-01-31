1cebffd2ffc4013901545ff7e7b3cd454a00262f
Yugo Matsuda <yugo1997zazen@gmail.com>
2026-01-31 16:02:00 -0500

    Add CropableImagePicker

---
+++ frontend/iosApp/UIComponents/Sources/UIComponents/Components/CropableImagePicker.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+import UIKit
+
+struct CropableImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.dismiss) private var dismiss
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CropableImagePicker
+
+        init(_ parent: CropableImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+        ) {
+            if let image = info[.editedImage] as? UIImage {
+                parent.image = image
+            } else if let image = info[.originalImage] as? UIImage {
+                parent.image = image
+            }
+            parent.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.dismiss()
+        }
+    }
+}
